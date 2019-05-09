55404ba8b68ff10beb7474c4b0add959f818cf8b
m810459675 <48501876+m810459675@users.noreply.github.com>
2019-05-09 19:28:33 +0800

    能够读取本地train_set.csv文件进行训练

--- 线性回归/script.js
+++ 线性回归/script.js
@@ -3,6 +3,7 @@
  * and cleaned of missing data.
  */
 /*jshint esversion:8*/
+/*
 async function getData() {
   const carsDataReq = await fetch('https://storage.googleapis.com/tfjs-tutorials/carsData.json');
   const carsData = await carsDataReq.json();
@@ -11,28 +12,110 @@ async function getData() {
     horsepower: car.Horsepower,
   }))
   .filter(car => (car.mpg != null && car.horsepower != null));
-
+   console.log(cleaned);
   return cleaned;
+}
+*/
+/* //an example to read .csv file
+Papa.parse('./train_set.csv', {
+	download:true,
+	complete: function(results) {
+		//console.log("Finished:", results.data);
+		let data=results.data;
+		//console.log(data.length);
+		for(let i=0;i<data.length;i++){
+			var item=data[i];
+			console.log(item);
+			//console.log(item[0]);
+		}
+
+	}
+});
+*/
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+
+
+
+async function getData(){
+  const value=[];
+	var num=0;
+/*
+	Papa.parse('./train_set.csv', {
+		download:true,
+		complete: function(results) {
+
+			//console.log("Finished:", results.data);
+			dt=results.data;
+			//console.log(data.length);
+			for(let i=0;i<dt.length-1;i++){
+				var item=dt[i];
+				if (item=="")
+				continue;
+				else
+				num++;
+				//console.log(typeof item[0]);
+				//console.log(i,item[0],item[1]);
+				//value.push([parseFloat(item[0]),parseFloat(item[1])]);
+				value.push([Number(item[0]),Number(item[1])]);
+
+			}
+		}
+	});
+console.log(value);
+*/
+/*
+d3.csv("./train_set.csv", function(error, data) {
+  if (error) throw error;
+  console.log("71"+data);
+});
+*/
+const data = await d3.csv("train_set.csv");
+
+console.log(data.length);
+console.log(data[1].x);
+for (let i=0;i<data.length;i++){
+	if(data[i].x=="") continue;
+	value.push([parseFloat(data[i].x),parseFloat(data[i].y)]);
+}
+console.log(value);
+/*
+  for (let x=-20;x<20;x+=0.3){
+    value.push([x,x*x+x+getRndInteger(10,20)]);
+  }
+	console.log(typeof value);
+console.log(value);
+*/
+const values=value.map(v=>({
+  x:v[0],
+  y:v[1],
+}));
+
+  return values;
 }
 
 async function run() {
   // Load and plot the original input data that we are going to train on.
   const data = await getData();
+
   const values = data.map(d => ({
-    x: d.horsepower,
-    y: d.mpg,
+    x: d.x,
+    y: d.y,
   }));
 
+//console.log(data);
   tfvis.render.scatterplot(
-    {name: 'Horsepower v MPG'},
+    {name: 'lineaner regression'},
     {values},
     {
-      xLabel: 'Horsepower',
-      yLabel: 'MPG',
+      xLabel: 'x',
+      yLabel: 'y',
       height: 300
     }
   );
-
+//console.log(values);
   // More code will be added below
   // Create the model
 const model = createModel();
@@ -60,13 +143,16 @@ function createModel() {
 
   // Add a single hidden layer
   model.add(tf.layers.dense({inputShape: [1], units: 1, useBias: true}));
+  model.add(tf.layers.dense({units: 1024, useBias: true}));
+  model.add(tf.layers.dense({units: 1024, activation: 'relu'}));
+  /*
+  model.add(tf.layers.dense({units: 1024, activation: 'relu'}));
   model.add(tf.layers.dense({units: 512, activation: 'relu'}));
   model.add(tf.layers.dense({units: 256, activation: 'relu'}));
   model.add(tf.layers.dense({units: 128, activation: 'relu'}));
   model.add(tf.layers.dense({units: 64, activation: 'relu'}));
   model.add(tf.layers.dense({units: 32, activation: 'relu'}));
-  model.add(tf.layers.dense({units: 16, activation: 'relu'}));
-  model.add(tf.layers.dense({units: 8, activation: 'relu'}));
+  */
   // Add an output layer
   model.add(tf.layers.dense({units: 1, useBias: true}));
 
@@ -89,8 +175,8 @@ function convertToTensor(data) {
     tf.util.shuffle(data);
 
     // Step 2. Convert data to Tensor
-    const inputs = data.map(d => d.horsepower);
-    const labels = data.map(d => d.mpg);
+    const inputs = data.map(d => d.x);
+    const labels = data.map(d => d.y);
 
     const inputTensor = tf.tensor2d(inputs, [inputs.length, 1]);
     const labelTensor = tf.tensor2d(labels, [labels.length, 1]);
@@ -125,7 +211,7 @@ async function trainModel(model, inputs, labels) {
   });
 
   const batchSize = 16;
-  const epochs = 50;
+  const epochs = 35;
 
   return await model.fit(inputs, labels, {
     batchSize,
@@ -169,16 +255,16 @@ function testModel(model, inputData, normalizationData) {
   });
 
   const originalPoints = inputData.map(d => ({
-    x: d.horsepower, y: d.mpg,
+    x: d.x, y: d.y,
   }));
 
 
   tfvis.render.scatterplot(
-    {name: 'Model Predictions vs Original Data'},
+    {name: 'test model'},
     {values: [originalPoints, predictedPoints], series: ['original', 'predicted']},
     {
-      xLabel: 'Horsepower',
-      yLabel: 'MPG',
+      xLabel: 'x',
+      yLabel: 'y',
       height: 300
     }
   );
